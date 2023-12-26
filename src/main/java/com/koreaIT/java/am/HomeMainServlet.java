8114a6f2ca517275b4a2d8d670d63701405f0f2a
wdwdwq <qkenr7278@gmail.com>
2023-12-26 19:48:02 +0900

    스프립틀릿 기초 사용법

--- src/main/java/com/koreaIT/java/am/HomeMainServlet.java
+++ src/main/java/com/koreaIT/java/am/HomeMainServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @WebServlet("/home/Main")
 public class HomeMainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+    //
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html; charset=UTF-8;");
--- src/main/java/com/koreaIT/java/am/HomePrintServlet.java
+++ src/main/java/com/koreaIT/java/am/HomePrintServlet.java
@@ -12,16 +12,9 @@ public class HomePrintServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/jsp/home/print.jsp").forward(request, response);
+		request.getRequestDispatcher("/jsp/home/print.jsp").forward(request, response);//서블릿이 하는일은 jsp에 일넘기는 일을
 	}
 
 }
 
-//		.append(request.getContextPath())
-// 여기를 지정을 안해줘서 아직 내 프로젝트 이름으로 들어간다
-
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//
-//		doGet(request, response);
-//	}
+ 
